1a26b8acb6d1abbd8f0b46bb2c56297977d138c9
Eduard Bissell <edd.bissell@hotmail.co.uk>
2024-05-13 16:36:40 +0100

    refactor: extract getById

--- controllers/item.controllers.go
+++ controllers/item.controllers.go
@@ -28,28 +28,7 @@ func (ic *ItemController) Get(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	id, err := strconv.ParseInt(vars["id"], 10, 64)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte("error id must be int"))
-		return
-	}
-
-	item, err := ic.queries.SelectItem(ic.ctx, id)
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-		w.Write([]byte("controllers:ItemController:Get - SelectItem " + string(err.Error())))
-		return
-	}
-
-	itemJson, err := json.Marshal(item)
-	if err != nil {
-		w.WriteHeader(500)
-		w.Write([]byte("controllers:ItemController:Get - json.Marshal " + string(err.Error())))
-		return
-	}
-
-	w.Write(itemJson)
+	ic.getById(w, idStr)
 }
 
 func (ic *ItemController) list(w http.ResponseWriter) {
@@ -70,3 +49,27 @@ func (ic *ItemController) list(w http.ResponseWriter) {
 	w.Write(itemJson)
 }
 
+func (ic *ItemController) getById(w http.ResponseWriter, id string) {
+	idInt, err := strconv.ParseInt(id, 10, 64)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error id must be int"))
+		return
+	}
+
+	item, err := ic.queries.SelectItem(ic.ctx, idInt)
+	if err != nil {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("controllers:ItemController:Get - SelectItem " + string(err.Error())))
+		return
+	}
+
+	itemJson, err := json.Marshal(item)
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("controllers:ItemController:Get - json.Marshal " + string(err.Error())))
+		return
+	}
+
+	w.Write(itemJson)
+}
